8e6ac64521e8ed03c81f75a73ea6152287d66178
Vinny Meller <vinnymeller@proton.me>
2024-06-08 22:10:05 -0400

    add tanstack start

---
+++ tanstack-router-quickstart/src/routes/__root.tsx
@@ -0,0 +1,30 @@
+import React, { Suspense } from 'react'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+
+const TanStackRouterDevtools = process.env.NODE_ENV === "production" ? () => null
+  : React.lazy(() =>
+    import("@tanstack/router-devtools").then((res) => ({
+      default: res.TanStackRouterDevtools,
+    })),
+  )
+
+export const Route = createRootRoute({
+  component: Root,
+})
+
+function Root() {
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">Home</Link>{' '}
+        <Link to="/about" className="[&.active]:font-bold">About</Link>{' '}
+      </div>
+      <hr />
+      <Outlet />
+      <Suspense>
+        <TanStackRouterDevtools initialIsOpen={false} />
+      </Suspense>
+    </>
+  )
+
+}
